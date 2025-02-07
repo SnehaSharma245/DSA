1e6a2d8abe162b16232a2dd95bc8df053809a421
Sneha Sharma <snehav2109@gmail.com>
2025-02-07 18:47:53 +0530

    square root with precision using binary search

--- Lec014 - Binary_Search_Problem_Set_2/square_root_more_precision.cpp
+++ Lec014 - Binary_Search_Problem_Set_2/square_root_more_precision.cpp
@@ -2,12 +2,56 @@
 using namespace std;
 long long int intSquareRoot(int x)
 {
+    int s = 0;
+    int e = x;
+    int ans = -1;
+    long long int mid = s + (e - x) / 2;
+    while (s <= e)
+    {
+        long long int square = mid * mid;
+        if (square == x)
+        {
+            return mid;
+        }
+        else if (square < x)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+double morePrecision(int n, int precision, int tempSol)
+{
+    double factor = 1;
+    double ans = tempSol;
+    for (int i = 0; i < precision; i++)
+    {
+        // 0.1 add in first loop
+        // 0.01 add in second loop
+        // 0.001 add in third loop
+        //  and so on....
+        factor = factor / 10;
+
+        for (double j = ans; j * j < n; j = j + factor)
+        {
+            ans = j;
+        }
+    }
+    return ans;
 }
 int main()
 {
     int x;
     cout << "Enter a number: ";
     cin >> x;
-    int intSol;
+    long long int intSol = intSquareRoot(x);
+    double doubleSol = morePrecision(x, 3, intSol);
+    cout << "Square root with more precision is " << doubleSol << endl;
     return 0;
 }
